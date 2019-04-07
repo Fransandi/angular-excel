ba78d828211986596448f2be1bc0f8b5d33b2bef
Fran <fransandi@gmail.com>
2019-04-07 17:34:05 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'xls';
+
+  arrayBuffer: any;
+  file: File;
+  JSONObject = {
+    object: {},
+    string: ''
+  };
+
+  constructor() { }
+
+  incomingfile(event) {
+    this.file = event.target.files[0];
+  }
+
+  upload() {
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      const data = new Uint8Array(this.arrayBuffer);
+      const arr = new Array();
+      for (let i = 0; i !== data.length; ++i) { arr[i] = String.fromCharCode(data[i]); }
+      const bstr = arr.join('');
+      const workbook = XLSX.read(bstr, { type: 'binary' });
+      const first_sheet_name = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[first_sheet_name];
+      const JSON_Object = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+
+      this.JSONObject.object = JSON_Object; //Data in JSON Format
+      this.JSONObject.string = JSON.stringify(JSON_Object); //Data in String Format
+
+      console.log('JSON object:', this.JSONObject.object);
+    };
+    fileReader.readAsArrayBuffer(this.file);
+  }
 }
